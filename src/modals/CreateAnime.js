afab864a1bc63f7c073f7232ffe98c8f8edcb80d
Vanya <vanyailnitsk@yandex.ru>
2023-11-26 23:58:43 +0300

    CreateAnime form submit functionality

--- src/modals/CreateAnime.js
+++ src/modals/CreateAnime.js
@@ -10,6 +10,8 @@ const CreateAnime = () => {
         description: '',
         folderName: '',
     });
+    const [banner,setBanner] = useState(null)
+    const [card,setCard] = useState(null)
     const [status, setStatus] = useState('');
 
     const handleInputChange = (event) => {
@@ -20,10 +22,32 @@ const CreateAnime = () => {
         });
     };
 
+    const handleBannerChange = (event) => {
+        setBanner(event.target.files[0]);
+    };
+
+    const handleCardChange = (event) => {
+        setCard(event.target.files[0]);
+    };
+
+    function getFormData() {
+        if (animeData.title && animeData.folderName && card && banner) {
+            const formData = new FormData();
+            formData.append('banner', banner);
+            formData.append('card', card);
+            for (const key in animeData) {
+                formData.append(key, animeData[key]);
+            }
+            return formData;
+        }
+    }
     const handleSubmit = () => {
-        createAnime(animeData)
-            .then(data => setStatus("Аниме успешно создано"))
-            .catch(error => setStatus("Ошибка при создании аниме"));
+        const formData = getFormData()
+        if (formData) {
+            createAnime(formData)
+                .then(data => setStatus("Аниме успешно создано"))
+                .catch(error => setStatus("Ошибка при создании аниме"));
+        }
     };
 
     return (
@@ -71,11 +95,11 @@ const CreateAnime = () => {
                 <div className="upload-pictures">
                     <div className="anime-card">
                         <span>Anime card</span>
-                        <input type="file" />
+                        <input type="file" onChange={handleCardChange}/>
                     </div>
                     <div className="picture">
-                        <span>Anime picture</span>
-                        <input type="file" />
+                        <span>Anime banner</span>
+                        <input type="file" onChange={handleBannerChange}/>
                     </div>
                 </div>
                 <div className="create-btn">
