91cf5c635764e68ddd37e74303951743d8fc3f59
Николай Ильницкий <kolyailnitsk@Nikolajs-MacBook-Air.local>
2024-02-03 01:55:25 +0300

    cards from base

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,16 +3,17 @@ import '../styles/Navigation.css'
 import homeIcon from '../images/home.png'
 import searchIcon from '../images/search.png'
 import {useNavigate} from "react-router-dom";
+import {HOME_ROUTE, SEARCH__ROUTE} from "../navigation/routes";
 const Navigation = () => {
     const navigate = useNavigate();
     return (
         <div className='navigation__wrapper'>
             <div className="nav__block">
-                <div className='nav_title' onClick={() => navigate('/')}>
+                <div className='nav_title' onClick={() => navigate(HOME_ROUTE)}>
                     <img style={{width:26}} src={homeIcon} alt=""/>
                     <span>Home</span>
                 </div>
-                <div className='nav_title'>
+                <div className='nav_title' onClick={() => navigate(SEARCH__ROUTE)}>
                     <img style={{width:26}} src={searchIcon} alt=""/>
                     <span>Search</span>
                 </div>
