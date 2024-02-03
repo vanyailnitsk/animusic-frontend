59a77ff73816614ccb4db1bd50e6e69965c75400
Николай Ильницкий <kolyailnitsk@Nikolajs-MacBook-Air.local>
2024-02-03 22:41:35 +0300

    scrollbar

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,9 +6,6 @@ import DataCore from "./DataCore";
 const MainContent = ({page}) => {
     return (
         <div className='main__content__wrapper'>
-            <div className='logo'>
-                <span>Animusic</span>
-            </div>
             <div className='profile__picture__wrapper'>
                 <img src={avatar} alt="" style={{width:'100%',display:'block'}}/>
             </div>
--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -17,7 +17,10 @@ const Homepage = () => {
 
     return (
         <div className="home__page__wrapper">
-            <div style={{width:'98%',marginInline:'auto'}}>
+            <div className='logo'>
+                <span>Animusic</span>
+            </div>
+            <div style={{width:'98%',marginInline:'auto',marginTop:10}}>
                 Welcome to Animusic – your cozy corner in the world of anime music! Here, every sound is like a note in a story, every melody is like a chapter in your imaginary adventure. Discover the magic of anime sounds, immerse yourself in the musical worlds of your favorite TV series and movies. Enjoy a collection of the most mesmerizing and exciting compositions created by talented composers, especially for the anime world. From epic soundtracks to exciting openings and endings, everyone will find something special here.
             </div>
             {!isLoading
