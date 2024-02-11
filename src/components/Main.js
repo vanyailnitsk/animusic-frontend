b366882c63c3ecae8c09aca02491a86268e3a348
Николай Ильницкий <kolyailnitsk@Nikolajs-MacBook-Air.local>
2024-02-11 15:48:24 +0300

    hidden music player on landscape orientation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../styles/Main.css'
 import Navigation from "./Navigation";
 import MediaLibrary from "./MediaLibrary";
 import MainContent from "./MainContent";
+
 
 const Main = ({page}) => {
     const [menuActive,setMenuActive] = useState(false)
