024586604a2ae0b97e367a19f019b36d4dcc9425
Николай Ильницкий <kolyailnitsk@Nikolajs-MacBook-Air.local>
2024-04-21 11:42:18 +0300

    удалил аниме и саундрек менеджер

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -16,8 +16,6 @@ import {
 } from "./routes";
 import AlbumPage from "../pages/AlbumPage/AlbumPage";
 import MusicPlayer from "../components/MusicPlayer/MusicPlayer";
-import SoundtrackManager from "../pages/SoundtrackManager";
-import AnimeManager from "../pages/AnimeManager";
 import SearchPage from "../pages/SearchPage/SearchPage";
 import Login from "../components/Login/Login";
 import SignUp from "../components/SignUp/SignUp";
@@ -39,9 +37,7 @@ function AppRouter() {
                         <Route element={<PrivateRoute/>}>
                             <Route path={HOME_ROUTE} element={<Main page={<Homepage/>}/>}/>
                             <Route path={ANIME_ROUTE} element={<Main page={<AnimePage/>}/>}/>
-                            <Route path={ANIME_MANAGE} element={<Main page={<AnimeManager/>}/>}/>
                             <Route path={ALBUM_ROUTE} element={<Main page={<AlbumPage/>}/>}/>
-                            <Route path={SOUNDTRACK_MANAGE} element={<Main page={<SoundtrackManager/>}/>}/>
                             <Route path={SEARCH_ROUTE} element={<Main page={<SearchPage/>}/>}/>
                             <Route path={COLLECTION} element={<Main page={<UserPlaylistPage/>}/>}/>
                         </Route>
