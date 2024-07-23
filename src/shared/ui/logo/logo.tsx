52d64bafca0118e84a466e3228d9b77ddefe1a5e
kolyailnitsk <kolya.ilnitsk@yandex.ru>
2024-07-23 18:46:15 +0300

    переделан хук useFetching и логика с запросом всех аниме перенесена в cardlist

---
+++ src/shared/ui/logo/logo.tsx
@@ -0,0 +1,10 @@
+import styles from './logo.module.css'
+
+export const Logo = () => {
+    return (
+        <div className={styles.logo}>
+            <span>Animusic</span>
+        </div>
+    );
+};
+
